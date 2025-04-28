77cb6c7c8155d630a7b60acc936741299df04add
maxidaniele <maxidanielemg@gmail.com>
2025-04-28 20:21:32 -0300

    probando el cambio

---
+++ frontend/src/Components/Formulario.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+import { useState } from 'react'
+import Alerta from './Alerta'
+
+const Formulario = () => {
+    const [nombre, setNombre] = useState('')
+<<<<<<< HEAD:frontend/src/Formulario.jsx
+    const [telefono, setTelefono] = useState('')
+=======
+    const [telefono, setTelefono] = useState(0)
+>>>>>>> 0011fa56c37dfbb0a0633a74ef79c251f1222450:frontend/src/Components/Formulario.jsx
+    const [email, setEmail] = useState('')
+    const [domicilio, setDomicilio] = useState('')
+
+    const [alerta, setAlerta] = useState({})
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        //validar formulario
+        if([nombre,telefono,email,domicilio].includes('')){
+            setAlerta({
+                msg: 'Todos los campos son obligatorios',
+                error: true
+            })
+            return;
+        }
+        
+        setAlerta({})
+        
+    }
+
+    const {msg} = alerta
+
+  return (
+    <>
+
+    <form 
+    className='text-left bg-white w-lg py-10 px-5 mb-10 shadow-md rounded-md'
+    onSubmit={handleSubmit}
+    >
+    
+    {msg & <Alerta alerta = {alerta}/>}
+    
+    <p className='text-lg text-left mb-10 uppercase font-bold'>
+        Crear un Nuevo Cliente
+    </p>
+
+        <div className='mb-5'>
+            <label 
+                htmlFor='nombre'
+                className='text-blue-900 uppercase font-bold'
+            >Nombre</label>
+            <input 
+            id='nombre' 
+            type='text' 
+            placeholder='Nombre Cliente' 
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            value={nombre}
+            onChange={e => setNombre(e.target.value)}
+            />
+
+        </div>
+        <div className='mb-5'>
+            <label 
+                htmlFor='telefono'
+                className='text-blue-900 uppercase font-bold'
+            >Telefono</label>
+            <input 
+            id='telefono' 
+            type='number' 
+            placeholder='Telefono' 
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            value={telefono}
+            onChange={e => setTelefono(e.target.value)}
+            />
+
+        </div>
+        <div className='mb-5'>
+            <label 
+                htmlFor='email'
+                className='text-blue-900 uppercase font-bold'
+            >Email</label>
+            <input 
+            id='email' 
+            type='text' 
+            placeholder='Email Cliente' 
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            />
+
+        </div>
+        <div className='mb-5'>
+            <label 
+                htmlFor='domicilio'
+                className='text-blue-900 uppercase font-bold'
+            >Domicilio</label>
+            <input 
+            id='domicilio' 
+            type='text' 
+            placeholder='Domicilio cliente' 
+            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+            value={domicilio}
+            onChange={e => setDomicilio(e.target.value)}
+            />
+
+        </div>
+
+        <input
+            type='submit'
+            className='bg-blue-800 p-3 w-full mt-6 text-white font-bold uppercase hover:bg-blue-950 rounded-md 
+            cursor-pointer transition-colors'
+            value="Agregar Cliente"
+
+        />
+    </form>
+
+    </>
+
+  )
+}
+
+export default Formulario
---
+++ frontend/src/Components/ListaDeRegistros.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useState } from "react";
+
+
+
+const ListaDeRegistros = () => {
+  const [registros, setRegistros] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // Función para obtener los registros del backend
+  const fetchRegistros = async () => {
+    setLoading(true); // Mostrar mensaje de carga
+    try {
+      const response = await fetch("http://127.0.0.1:5000/api/clientes"); // Cambia la URL según tu endpoint
+      if (!response.ok) {
+        throw new Error("Ups! No se pudo obtener la información.");
+      }
+      const datos = await response.json();
+      setRegistros(datos);
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setLoading(false); // Ocultar mensaje de carga
+    }
+  };
+
+  return (
+    <div>
+      <h1>Listado de Registros</h1>
+      {/* Botón para traer los registros */}
+      <button onClick={fetchRegistros}  className="btnActualizar" style={{ marginBottom: "20px" }}>
+        Actualizar
+      </button>
+
+      {loading ? (
+        <p>Un momento... Los registros se están cargando.</p>
+      ) : registros.length > 0 ? (
+        <table border="1" style={{ width: "100%", textAlign: "left" }}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Teléfono</th>
+              <th>Email</th>
+              <th>Dirección</th>
+            </tr>
+          </thead>
+          <tbody>
+            {registros.map((registro) => (
+              <tr key={registro.id}>
+                <td>{registro.id}</td>
+                <td>{registro.nombre}</td>
+                <td>{registro.telefono}</td>
+                <td>{registro.email}</td>
+                <td>{registro.direccion}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No hay registros disponibles.</p>
+      )}
+    </div>
+  );
+};
+
+export default ListaDeRegistros;
