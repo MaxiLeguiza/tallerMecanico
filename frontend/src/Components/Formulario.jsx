f3624754f65bd56c47312c72d7f9c9905a54c16b
MaxiLeguiza <leguimanfire17@gmail.com>
2025-04-28 21:33:18 -0300

    Corrección de Formulario

--- frontend/src/Components/Formulario.jsx
+++ frontend/src/Components/Formulario.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-//import Alerta from './Alerta';
+import Alerta from './Alerta';
 
 
 const Formulario = () => {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-<<<<<<< HEAD
-import Formulario from './Components/formulario.jsx'
+import Formulario from './Components/Formulario.jsx'
 import ListaDeRegistros from './Components/ListaDeRegistros.jsx'
 import './Style/main.css'
-=======
-import Formulario from './Components/Formulario'
->>>>>>> 49547cc30067a8628f5f1f381b4e91c2a3e92918
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+   
     <div className="grid-container">
-    <div><Formulario/></div>
-    <div><ListaDeRegistros/></div>
+      <div> <Formulario/></div>
+      <div><ListaDeRegistros/></div>
     </div>
   </StrictMode>,
 )
